2834ebc558ca99187cefc245fcc0df17e6afdc78
Quinn Jonas <qj@nevada.unr.edu>
2014-10-01 05:44:22 -0700

    Create loader.h

---
+++ Assignment05/src/loader.h
@@ -0,0 +1,13 @@
+// this is the header file for the shader loader class
+
+class loader{
+public:
+loader( const char* uName);
+~loader();
+char* getData();
+
+private:
+ char* fName;
+ char* data;
+
+};
